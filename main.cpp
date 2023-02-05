643abebbf151d2c84b3905c023e2d15823d4f048
mrkakawiteo <loveyourswain@gmail.com>
2023-02-05 12:00:40 +0200

    creating function

--- main.cpp
+++ main.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
 #include <vector>
 #include <windows.h>
+#include <algorithm>
+#include <sstream>
 
+std::string findFirstUniqueStr(std::string& input) {
+    std::istringstream sstream(input);
+    std::string word;
+    std::string result = "";
+    while (sstream >> word) {
+        for (auto symbol: word) {
+            int symbolFrequency = std::count(word.begin(), word.end(), symbol);
+            if (symbolFrequency == 1) {
+                result += symbol;
+                break;
+            }
+        }
+    }
+    return result;
+}
 
 int main() {
     SetConsoleOutputCP(CP_UTF8);
-    std::cout << "Дарова, перевірка Гіт коміту"<< '\n';
-    std::cout << "Друга перевірка коміту";
     return 0;
 }
