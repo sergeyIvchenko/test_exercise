72249d24c586114622815b76ede1140465c6eb0c
mrkakawiteo <loveyourswain@gmail.com>
2023-02-05 11:56:52 +0200

    first run

---
+++ cmake-build-debug/CMakeFiles/test_exercise.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_exercise.dir/main.cpp.obj"
+  "libtest_exercise.dll.a"
+  "test_exercise.exe"
+  "test_exercise.exe.manifest"
+  "test_exercise.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_exercise.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+#include <windows.h>
+
+
+int main() {
+    SetConsoleOutputCP(CP_UTF8);
+    std::cout << "Дарова, перевірка Гіт коміту"<< '\n';
+    return 0;
+}
