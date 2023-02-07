ffbc2147305e51e7eca0190e5c689179b0ef40a4
mrkakawiteo <loveyourswain@gmail.com>
2023-02-07 05:32:26 +0200

    completed slow function

--- main.cpp
+++ main.cpp
@@ -4,25 +4,29 @@
 #include <algorithm>
 #include <sstream>
 
+
+char getFirstUniqueSymbol(std::string word){
+    for (auto symbol: word) {
+        int symbolFrequency = std::count(word.begin(), word.end(), symbol);
+        if (symbolFrequency == 1) {
+            return symbol;
+        }
+    }
+    return ' ';
+}
 std::string findFirstUniqueStr(std::string& input) {
     std::istringstream sstream(input);
     std::string word;
     std::string result = "";
     while (sstream >> word) {
-        for (auto symbol: word) {
-            int symbolFrequency = std::count(word.begin(), word.end(), symbol);
-            if (symbolFrequency == 1) {
-                result += symbol;
-                break;
-            }
-        }
+        result += getFirstUniqueSymbol(word);
     }
     return result;
 }
 
-/*Повільніший метод:шукаємо з першого символу повторення, якщо є — видаляємо їх, інакше — це і є перший унікальний символ*/
+///Повільніший метод:шукаємо з першого символу повторення, якщо є — видаляємо їх, інакше — це і є перший унікальний символ
 char getFirstUniqueSymbolSlow(std::string strOfUnique){
-
+    return getFirstUniqueSymbol(strOfUnique);
 }
 
 /*Швидший метод: відбувається всього 1 прохід по рядку унікальних символів слів початкового речення,
@@ -36,17 +40,22 @@ char getFirstUniqueSymbolFast(std::string strOfUnique){
             symbolsPos.push_back(symbol);
         }
     }
-    char result = *std::find_if(symbolsPos.begin(), symbolsPos.end(),[&symbolFreqMap](char symbol){
-       if(symbolFreqMap[symbol] == 1)return true;
+    char result = ' ';
+    std::find_if(symbolsPos.begin(), symbolsPos.end(),[&symbolFreqMap,&result](char symbol){
+       if(symbolFreqMap[symbol] == 1){
+           result = symbol;
+           return true;
+       }
        else return false;
     });
+    if(result == ' ')std::cout << "Унікального першого символу слів речення не знайдено";
     return result;
 }
 
 int main() {
     SetConsoleOutputCP(CP_UTF8);
-    std::string str = "Have a Hice ayy puppy uf course";
+    std::string str = "C makes it easy for you to shoot yourself in the foot. C++ makes that harder, but when you do, it blows away your whole leg. (с) Bjarne Stroustrup";
     std::string uniqueSymbols = findFirstUniqueStr(str);
-    std::cout << getFirstUniqueSymbolFast(uniqueSymbols);
+    std::cout << getFirstUniqueSymbolSlow(uniqueSymbols);
     return 0;
 }
