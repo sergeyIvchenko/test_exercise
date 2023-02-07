9563812250eb315e927041f8586e995d6b176d7d
mrkakawiteo <loveyourswain@gmail.com>
2023-02-07 04:38:29 +0200

    completed fast function

--- main.cpp
+++ main.cpp
@@ -20,7 +20,33 @@ std::string findFirstUniqueStr(std::string& input) {
     return result;
 }
 
+/*Повільніший метод:шукаємо з першого символу повторення, якщо є — видаляємо їх, інакше — це і є перший унікальний символ*/
+char getFirstUniqueSymbolSlow(std::string strOfUnique){
+
+}
+
+/*Швидший метод: відбувається всього 1 прохід по рядку унікальних символів слів початкового речення,
+ * потім один прохід по масиву, що надає порядок додавання символів у мапу*/
+char getFirstUniqueSymbolFast(std::string strOfUnique){
+    std::unordered_map<char,int> symbolFreqMap;
+    std::vector<char> symbolsPos;
+    for(auto symbol:strOfUnique){
+        symbolFreqMap[symbol]++;
+        if(symbolFreqMap[symbol] == 1){
+            symbolsPos.push_back(symbol);
+        }
+    }
+    char result = *std::find_if(symbolsPos.begin(), symbolsPos.end(),[&symbolFreqMap](char symbol){
+       if(symbolFreqMap[symbol] == 1)return true;
+       else return false;
+    });
+    return result;
+}
+
 int main() {
     SetConsoleOutputCP(CP_UTF8);
+    std::string str = "Have a Hice ayy puppy uf course";
+    std::string uniqueSymbols = findFirstUniqueStr(str);
+    std::cout << getFirstUniqueSymbolFast(uniqueSymbols);
     return 0;
 }
